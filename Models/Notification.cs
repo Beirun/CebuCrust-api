19f2db98d10eb6fee6c997b0659a42a0523a5547
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:51 +0800

    added Notification model

---
+++ Models/Notification.cs
@@ -0,0 +1,17 @@
+﻿using CebuCrust_api.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CebuCrust_api.Models
+{
+    public class Notification
+    {
+        [Key] public int NotificationId { get; set; }
+        public int UserId { get; set; }
+        [Required] public string NotificationMessage { get; set; } = "";
+        public string? NotificationStatus { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; } = null!;
+    }
+}
