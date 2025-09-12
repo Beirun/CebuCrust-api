b4396437ee9376d5ce404d3d04e5b1b7a81fae7d
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:50 +0800

    added Favorite model

---
+++ Models/Favorite.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.Models;
+using System;
+namespace CebuCrust_api.Models
+{
+    public class Favorite
+    {
+        public int UserId { get; set; }
+        public int PizzaId { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; } = null!;
+        public Pizza Pizza { get; set; } = null!;
+    }
+}
