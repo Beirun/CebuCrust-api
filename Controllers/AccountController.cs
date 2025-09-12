aef95b1773eeceedaffe754c68cc74b82d4c5f13
Beirun <orillobernardjay@gmail.com>
2025-09-13 00:07:05 +0800

    updated AccountController

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -70,20 +70,5 @@ namespace CebuCrust_api.Controllers
         }
     }
 
-    public class RegisterRequest
-    {
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string? PhoneNo { get; set; }
-        public string Password { get; set; } = "";
-        public string ConfirmPassword { get; set; } = "";
-    }
-
-
-    public class LoginRequest
-    {
-        public string Email { get; set; } = "";
-        public string Password { get; set; } = "";
-    }
+    
 }
