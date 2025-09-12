c1048ed0c0f4f62c32374c2a47f7232ce32495f4
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:51 +0800

    added Role model

---
+++ Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CebuCrust_api.Models
+{
+    public class Role
+    {
+        [Key] public int RoleId { get; set; }
+        [Required] public string RoleName { get; set; } = "";
+
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
