7957762897d2560c4235891485f3062128b39d0f
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:50 +0800

    added Location model

---
+++ Models/Location.cs
@@ -0,0 +1,22 @@
+﻿using CebuCrust_api.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CebuCrust_api.Models
+{
+    public class Location
+    {
+        [Key] public int LocationId { get; set; }
+        public int UserId { get; set; }
+        [Required] public string LocationCity { get; set; } = "";
+        public string? LocationBrgy { get; set; }
+        public string? LocationStreet { get; set; }
+        public string? LocationHouseNo { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public DateTime? DateUpdated { get; set; }
+
+        public User User { get; set; } = null!;
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
