e4523e0f5d7281e178d24b92eb35f2edd04c5491
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:51 +0800

    added OrderList model

---
+++ Models/OrderList.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Models
+{
+    public class OrderList
+    {
+        public int OrderId { get; set; }
+        public int PizzaId { get; set; }
+        public int Quantity { get; set; }
+
+        public Order Order { get; set; } = null!;
+        public Pizza Pizza { get; set; } = null!;
+    }
+}
