673e7eaaa7e43f3d558b129902dd02c47602bdc7
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:51 +0800

    added Rating model

---
+++ Models/Rating.cs
@@ -0,0 +1,20 @@
+﻿using CebuCrust_api.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CebuCrust_api.Models
+{
+    public class Rating
+    {
+        [Key] public int RatingId { get; set; }
+        public int UserId { get; set; }
+        public int PizzaId { get; set; }
+        public int RatingValue { get; set; }
+        public string? RatingMessage { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public DateTime? DateUpdated { get; set; }
+
+        public User User { get; set; } = null!;
+        public Pizza Pizza { get; set; } = null!;
+    }
+}
