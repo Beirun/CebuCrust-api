5ead5d251724f3e51d397243f29674f2b51b83b1
Beirun <orillobernardjay@gmail.com>
2025-09-12 21:03:50 +0800

    added Cart model

---
+++ Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using CebuCrust_api.Models;
+using System;
+namespace CebuCrust_api.Models
+{
+    public class Cart
+    {
+        public int PizzaId { get; set; }
+        public int UserId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public DateTime? DateUpdated { get; set; }
+
+        public User User { get; set; } = null!;
+        public Pizza Pizza { get; set; } = null!;
+    }
+}
