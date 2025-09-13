8e3431a773b987e20d3dfb7ae0fbcf4e7a9c8600
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:39 +0800

    added LocationRequest service model

---
+++ ServiceModels/LocationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class LocationRequest
+    {
+        public int UserId { get; set; }
+        public string LocationCity { get; set; } = "";
+        public string? LocationBrgy { get; set; }
+        public string? LocationStreet { get; set; }
+        public string? LocationHouseNo { get; set; }
+    }
+}
