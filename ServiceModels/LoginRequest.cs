9e1e949ef9f02a43ebf76d438c3987fe900d2efc
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:40 +0800

    added LoginRequest service model

---
+++ ServiceModels/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace CebuCrust_api.ServiceModels
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
