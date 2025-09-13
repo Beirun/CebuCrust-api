c653054c2c690e2c0b1b086da0d117fa78e32769
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:36 +0800

    added IOrderService interface

---
+++ Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.Models;
+using CebuCrust_api.ServiceModels;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetByUserIdAsync(int userId);
+        Task<IEnumerable<Order>> GetAllAsync(); // Admin only
+        Task<Order> CreateAsync(Order order, IEnumerable<OrderItemRequest> items);
+        Task<Order?> UpdateStatusAsync(int orderId, string status);
+        Task<bool> DeleteAsync(int orderId);
+    }
+}
