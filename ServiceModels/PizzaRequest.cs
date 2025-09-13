9f64a7638b330d10993649887a4a85d5b2a83d29
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:41 +0800

    added PizzaRequest service model

---
+++ ServiceModels/PizzaRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class PizzaRequest
+    {
+        public string PizzaName { get; set; } = "";
+        public string PizzaDescription { get; set; } = "";
+        public string PizzaCategory { get; set; } = "";
+        public decimal PizzaPrice { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
