691a3d13c1a1ed86295866948413544ab8c5dc3d
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:35 +0800

    added IAccountService interface

---
+++ Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using CebuCrust_api.Models;
+using CebuCrust_api.ServiceModels;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<AuthResult> RegisterAsync(User u, string password, string confirmPassword);
+        Task<AuthResult?> LoginAsync(string email, string password);
+        string? Refresh(string refreshToken);
+    }
+}
