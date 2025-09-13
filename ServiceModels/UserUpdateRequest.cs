f9385f8598fbb65833483d72b69524d1a6e8c47f
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:43 +0800

    added UserUpdateRequest service model

---
+++ ServiceModels/UserUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class UserUpdateRequest
+    {
+        public string? UserFName { get; set; }
+        public string? UserLName { get; set; }
+        public string? UserEmail { get; set; }
+        public string? UserPhoneNo { get; set; }
+
+        public string? CurrentPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
+
+        public IFormFile? Image { get; set; }
+    }
+}
