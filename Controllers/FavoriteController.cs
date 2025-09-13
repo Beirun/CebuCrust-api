1d5f16729f4654a9f1184bc23f9e6b756ac0a909
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:52 +0800

    updated FavoriteController

--- Controllers/FavoriteController.cs
+++ Controllers/FavoriteController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using CebuCrust_api.Models;
-using CebuCrust_api.Services;
+using CebuCrust_api.ServiceModels;
+using CebuCrust_api.Interfaces;
 
 namespace CebuCrust_api.Controllers
 {
