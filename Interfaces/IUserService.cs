16d0714644459d1d87b0d13a3f697e94ce8501cc
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:38 +0800

    added IUserService interface

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.Models;
+using CebuCrust_api.ServiceModels;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> UpdateAsync(int id, UserUpdateRequest request);
+        Task<bool> DeleteAsync(int id);
+        Task SaveImageAsync(int userId, IFormFile file);
+    }
+}
