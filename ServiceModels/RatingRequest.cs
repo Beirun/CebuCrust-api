700081cc18b60e785775c2a9e22a81a6033e6e19
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:42 +0800

    added RatingRequest service model

---
+++ ServiceModels/RatingRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class RatingRequest
+    {
+        public int UserId { get; set; }
+        public int PizzaId { get; set; }
+        public int RatingValue { get; set; }
+        public string? RatingMessage { get; set; }
+    }
+}
