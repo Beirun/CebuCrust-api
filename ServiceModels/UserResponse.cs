94a59007e33e3f3a50927201b3f415b8b77a0498
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:43 +0800

    added UserResponse service model

---
+++ ServiceModels/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class UserResponse { 
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNo { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
