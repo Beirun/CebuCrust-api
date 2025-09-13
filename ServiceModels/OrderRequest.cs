38003d9d33642d7eceaf2c7670886471b7d91189
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:41 +0800

    added OrderRequest service model

---
+++ ServiceModels/OrderRequest.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CebuCrust_api.ServiceModels
+{
+    public class OrderRequest
+    {
+        public int UserId { get; set; }
+        public int LocationId { get; set; }
+        public string? OrderInstruction { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? OrderEstimate { get; set; }
+        public List<OrderItemRequest> Items { get; set; } = new();
+    }
+}
