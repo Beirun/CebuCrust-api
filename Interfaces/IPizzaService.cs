7e8a5bb978553104852b1f81b63ea5fddcedc5a3
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:37 +0800

    added IPizzaService interface

---
+++ Interfaces/IPizzaService.cs
@@ -0,0 +1,15 @@
+﻿using CebuCrust_api.Models;
+using CebuCrust_api.ServiceModels;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IPizzaService
+    {
+        Task<IEnumerable<PizzaResponse>> GetAllAsync();
+        Task<PizzaResponse?> GetByIdAsync(int id);
+        Task<Pizza> CreateAsync(Pizza p);
+        Task<Pizza?> UpdateAsync(int id, Pizza p);
+        Task<bool> DeleteAsync(int id);
+        Task SaveImageAsync(int pizzaId, IFormFile file);
+    }
+}
