075c4a73547f650ae533f65d41c60fc46a5e8024
Beirun <orillobernardjay@gmail.com>
2025-09-13 15:49:38 +0800

    added CartRequest service model

---
+++ ServiceModels/CartRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class CartRequest
+    {
+        public int UserId { get; set; }
+        public int PizzaId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
