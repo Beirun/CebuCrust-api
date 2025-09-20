dbd5d8c51e784934503bb393520e3750618275ef
Beirun <orillobernardjay@gmail.com>
2025-09-20 22:16:51 +0800

    Add PasswordResetRequest.cs

---
+++ ServiceModels/PasswordResetRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class PasswordResetRequest
+    {
+        public string ResetCode { get; set; }
+        public string NewPassword {  get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
