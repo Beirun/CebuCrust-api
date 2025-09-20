ab7ab1b1439f5ee2ef46313ecc7f7d98a1d7ab00
Beirun <orillobernardjay@gmail.com>
2025-09-20 22:16:51 +0800

    Add Reset model

---
+++ Models/Reset.cs
@@ -0,0 +1,11 @@
+﻿namespace CebuCrust_api.Models
+{
+    public class Reset
+    {
+        public int ResetId {  get; set; }
+        public int UserId { get; set; }
+        public string ResetCode { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
