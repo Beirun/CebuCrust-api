0b39363daaff0740298b29203ca3b367e4a40052
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:52 +0800

    Added TokenOptions

---
+++ Authentication/TokenOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CebuCrust_api.Authentication
+{
+    public class TokenOptions
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public int AccessExpireMinutes { get; set; } = 60;
+        public int RefreshExpireDays { get; set; } = 7;
+    }
+}
