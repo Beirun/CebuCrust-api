85c82cf4295d930000b7672a241c030f9f47c167
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:55 +0800

    Added IRatingService

---
+++ Interfaces/Services/IRatingService.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.ServiceModels;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IRatingService
+    {
+        Task<IEnumerable<RatingResponse>> GetAllAsync();
+        Task<RatingResponse?> GetByIdAsync(int id);
+        Task<IEnumerable<RatingResponse>> GetByPizzaIdAsync(int pizzaId);
+        Task<RatingResponse> CreateAsync(int userId, RatingRequest request);
+        Task<RatingResponse?> UpdateAsync(int id, RatingRequest request);
+        Task<bool> DeleteAsync(int id);
+    }
+}
