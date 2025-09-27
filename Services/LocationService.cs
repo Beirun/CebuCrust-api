e2420475051d15551daeae922a963c2bcc5c9373
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:50 +0800

    Updated LocationService

--- Services/LocationService.cs
+++ Services/LocationService.cs
@@ -1,70 +1,85 @@
-﻿using CebuCrust_api.Config;
+﻿using CebuCrust_api.Interfaces;
 using CebuCrust_api.Models;
-using CebuCrust_api.Interfaces;
+using CebuCrust_api.Repositories;
 using CebuCrust_api.ServiceModels;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CebuCrust_api.Services
 {
-
-
     public class LocationService : ILocationService
     {
-        private readonly AppDbContext _db;
-        public LocationService(AppDbContext db) => _db = db;
+        private readonly ILocationRepository _repo;
+        public LocationService(ILocationRepository repo) => _repo = repo;
 
-        public async Task<IEnumerable<Location>> GetByUserIdAsync(int userId) =>
-            await _db.Locations.Include(l => l.User)
-                               .AsNoTracking()
-                               .Where(l => l.UserId == userId)
-                               .ToListAsync();
+        public async Task<IEnumerable<LocationResponse>> GetByUserAsync(int uid)
+        {
+            var locs = await _repo.GetByUserAsync(uid);
+            return locs.Select(l => new LocationResponse
+            {
+                LocationId = l.LocationId,
+                LocationCity = l.LocationCity,
+                LocationBrgy = l.LocationBrgy,
+                LocationStreet = l.LocationStreet,
+                LocationHouseNo = l.LocationHouseNo
+            });
+        }
 
-        public async Task<Location> CreateAsync(LocationRequest request)
+        public async Task<LocationResponse> CreateAsync(int uid, LocationRequest request)
         {
             var loc = new Location
             {
-                UserId = request.UserId,
+                UserId = uid,
                 LocationCity = request.LocationCity,
                 LocationBrgy = request.LocationBrgy,
                 LocationStreet = request.LocationStreet,
                 LocationHouseNo = request.LocationHouseNo,
                 DateCreated = DateTime.UtcNow
             };
+            await _repo.AddLocationAsync(loc);
 
-            _db.Locations.Add(loc);
-            await _db.SaveChangesAsync();
-            return loc;
+            return new LocationResponse
+            {
+                LocationId = loc.LocationId,
+                LocationCity = loc.LocationCity,
+                LocationBrgy = loc.LocationBrgy,
+                LocationStreet = loc.LocationStreet,
+                LocationHouseNo = loc.LocationHouseNo
+            };
         }
 
-        public async Task<Location?> UpdateAsync(int id, LocationRequest request)
+        public async Task<LocationResponse?> UpdateAsync(int uid, int id, LocationRequest request)
         {
-            var existing = await _db.Locations.FindAsync(id);
+            var existing = await _repo.GetByIdAsync(uid, id);
             if (existing == null) return null;
 
-            existing.UserId = request.UserId;
             existing.LocationCity = request.LocationCity;
             existing.LocationBrgy = request.LocationBrgy;
             existing.LocationStreet = request.LocationStreet;
             existing.LocationHouseNo = request.LocationHouseNo;
             existing.DateUpdated = DateTime.UtcNow;
 
-            await _db.SaveChangesAsync();
-            return existing;
+            await _repo.UpdateLocationAsync(existing);
+
+            return new LocationResponse
+            {
+                LocationId = existing.LocationId,
+                LocationCity = existing.LocationCity,
+                LocationBrgy = existing.LocationBrgy,
+                LocationStreet = existing.LocationStreet,
+                LocationHouseNo = existing.LocationHouseNo
+            };
         }
 
-        public async Task<bool> DeleteAsync(int id)
+        public async Task<bool> DeleteAsync(int uid, int id)
         {
-            var existing = await _db.Locations.FindAsync(id);
+            var existing = await _repo.GetByIdAsync(uid, id);
             if (existing == null) return false;
 
-            _db.Locations.Remove(existing);
-            await _db.SaveChangesAsync();
+            await _repo.DeleteLocationAsync(existing);
             return true;
         }
     }
-    
 }
