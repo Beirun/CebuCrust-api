946e582003445f3ddb36585fc6bc69043d1ed916
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:54 +0800

    Added ICartService

---
+++ Interfaces/Services/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using CebuCrust_api.ServiceModels;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface ICartService
+    {
+        Task<IEnumerable<CartResponse>> GetByUserAsync(int userId);
+        Task<CartResponse> CreateAsync(int userId, CartRequest request);
+        Task<CartResponse?> UpdateAsync(int userId, CartRequest request);
+        Task<bool> DeleteAsync(int userId, int pizzaId);
+
+    }
+}
