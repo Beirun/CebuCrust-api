3cec670c7fc346d82282905a565eb1ac441f3347
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:57 +0800

    Added OrderResponse model

---
+++ ServiceModels/OrderResponse.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CebuCrust_api.ServiceModels
+{
+    public class OrderResponse
+    {
+        public int OrderId { get; set; }
+        public int LocationId { get; set; }
+        public string? OrderInstruction { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? OrderEstimate { get; set; }
+        public List<OrderItemResponse> Items { get; set; } = new();
+    }
+}
