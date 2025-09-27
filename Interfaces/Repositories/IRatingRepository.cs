a5eee669850d798975d2e01603b66604dfb01c6f
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added IRatingRepository

---
+++ Interfaces/Repositories/IRatingRepository.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IRatingRepository
+    {
+        Task<List<Rating>> GetAllAsync();
+        Task<Rating?> GetByIdAsync(int id);
+        Task<List<Rating>> GetByPizzaIdAsync(int pizzaId);
+        Task<Rating> AddAsync(Rating r);
+        Task<Rating?> UpdateAsync(Rating r);
+        Task<bool> DeleteAsync(Rating r);
+    }
+}
