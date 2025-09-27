37be4ffdd3f67e4ae531c2704554ab10bf0c3551
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:57 +0800

    Added NotificationResponse model

---
+++ ServiceModels/NotificationResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class NotificationResponse
+    {
+        public int NotificationId { get; set; }
+        public string? NotificationMessage { get; set; }
+
+        public string? NotificationStatus { get; set; }
+    }
+}
