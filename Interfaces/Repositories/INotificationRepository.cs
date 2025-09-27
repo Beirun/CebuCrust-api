5d5b46d655a1c8a0be81a308abd5a96cf43ef2f7
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added INotificationRepository

---
+++ Interfaces/Repositories/INotificationRepository.cs
@@ -0,0 +1,12 @@
+﻿using CebuCrust_api.Models;
+namespace CebuCrust_api.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task<List<Notification>> GetByUserAsync(int uid);
+        Task<Notification?> GetByIdAsync(int id);
+        Task AddNotificationAsync(Notification n);
+        Task UpdateNotificationAsync(Notification n);
+        Task DeleteNotificationAsync(Notification n);
+    }
+}
