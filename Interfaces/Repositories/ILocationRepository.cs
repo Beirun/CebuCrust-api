fc8d424e7c6c579bbf63d5228b8dcd42b84c7800
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added ILocationRepository

---
+++ Interfaces/Repositories/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface ILocationRepository
+    {
+        Task<List<Location>> GetByUserAsync(int uid);
+        Task<Location?> GetByIdAsync(int uid, int id);
+        Task AddLocationAsync(Location loc);
+        Task UpdateLocationAsync(Location loc);
+        Task DeleteLocationAsync(Location loc);
+    }
+}
