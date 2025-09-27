f1a5f1356515a0b2a82f16db77775f793b8655e9
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:52 +0800

    Added IAccountRepository

---
+++ Interfaces/Repositories/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<bool> EmailExistsAsync(string email);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<bool> IsFirstUserAsync();
+        Task<Role?> GetRoleByNameAsync(string roleName);
+        Task AddUserAsync(User u);
+    }
+}
