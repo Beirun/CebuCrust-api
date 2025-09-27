7af4e9d5197de54b77af42ac091b619a3a65473c
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added IPizzaRepository

---
+++ Interfaces/Repositories/IPizzaRepository.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IPizzaRepository
+    {
+        Task<List<Pizza>> GetAllAsync();
+        Task<Pizza?> GetByIdAsync(int id);
+        Task<Pizza> AddAsync(Pizza p);
+        Task<Pizza?> UpdateAsync(Pizza p);
+        Task<bool> DeleteAsync(Pizza p);
+    }
+}
