81f1360c3f77ef51aff53983f113b9a7feb26711
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:54 +0800

    Added IUserRepository

---
+++ Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(int id);
+        Task UpdateAsync(User u);
+        Task<bool> SoftDeleteAsync(User u);
+    }
+}
