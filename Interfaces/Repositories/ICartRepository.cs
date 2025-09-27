5eceac4c4e7402da8e4cd0e8d88dbf10fa6abe65
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:52 +0800

    Added ICartRepository

---
+++ Interfaces/Repositories/ICartRepository.cs
@@ -0,0 +1,12 @@
+﻿using CebuCrust_api.Models;
+namespace CebuCrust_api.Interfaces
+{
+    public interface ICartRepository
+    {
+        Task<List<Cart>> GetByUserAsync(int uid);
+        Task<Cart?> GetCartItemAsync(int uid, int pizzaId);
+        Task AddCartAsync(Cart cart);
+        Task UpdateCartAsync(Cart cart);
+        Task DeleteCartAsync(Cart cart);
+    }
+}
