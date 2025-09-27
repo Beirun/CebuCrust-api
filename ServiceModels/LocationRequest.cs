9502f62d9cede708df1b38a3ddb3e619b78637a1
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:49 +0800

    Updated LocationRequest model

--- ServiceModels/LocationRequest.cs
+++ ServiceModels/LocationRequest.cs
@@ -2,8 +2,7 @@
 {
     public class LocationRequest
     {
-        public int UserId { get; set; }
-        public string LocationCity { get; set; } = "";
+        public string? LocationCity { get; set; } 
         public string? LocationBrgy { get; set; }
         public string? LocationStreet { get; set; }
         public string? LocationHouseNo { get; set; }
