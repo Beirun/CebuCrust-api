c663e56b270ca3add7e744ba651d1dbaf654028b
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:46 +0800

    Updated FavoriteController

--- Controllers/FavoriteController.cs
+++ Controllers/FavoriteController.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using CebuCrust_api.ServiceModels;
+using CebuCrust_api.Interfaces;
+using CebuCrust_api.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using CebuCrust_api.ServiceModels;
-using CebuCrust_api.Interfaces;
+using System.Threading.Tasks;
 
 namespace CebuCrust_api.Controllers
 {
@@ -14,23 +15,23 @@ namespace CebuCrust_api.Controllers
         private readonly IFavoriteService _svc;
         public FavoriteController(IFavoriteService svc) => _svc = svc;
 
-        [HttpGet("{userId:int}")]
-        public async Task<IActionResult> GetByUserId(int userId) =>
-            Ok(await _svc.GetByUserIdAsync(userId));
+        private int UserId => ClaimsHelper.GetUserId(User);
+
+        [HttpGet("me")]
+        public async Task<IActionResult> GetByUser() =>
+            Ok(await _svc.GetByUserAsync(UserId));
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] FavoriteRequest request)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            var created = await _svc.CreateAsync(request);
-            return CreatedAtAction(nameof(GetByUserId), new { userId = created.UserId }, created);
+            var success = await _svc.CreateAsync(UserId, request);
+            return success ? Ok(request) : Conflict("Favorite already exists");
         }
 
-        [HttpDelete("{userId:int}/{pizzaId:int}")]
-        public async Task<IActionResult> Delete(int userId, int pizzaId) =>
-            await _svc.DeleteAsync(userId, pizzaId) ? NoContent() : NotFound();
+        [HttpDelete("{pizzaId:int}")]
+        public async Task<IActionResult> Delete(int pizzaId) =>
+            await _svc.DeleteAsync(UserId, pizzaId) ? NoContent() : NotFound();
     }
-
-    
 }
