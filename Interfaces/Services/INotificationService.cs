c832552b80d732089b9ec8b025c237a67afe871f
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:54 +0800

    Added INotificationService

---
+++ Interfaces/Services/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.ServiceModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<NotificationResponse>> GetByUserAsync(int userId);
+        Task<NotificationResponse> CreateAsync(int userId, NotificationRequest request);
+        Task<NotificationResponse?> UpdateStatusAsync(int id, string status);
+        Task<bool> DeleteAsync(int id);
+    }
+}
