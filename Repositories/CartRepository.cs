27401dd36339c30fe923fa0577d22402072014a9
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:55 +0800

    Added CartRepository

---
+++ Repositories/CartRepository.cs
@@ -0,0 +1,43 @@
+﻿using CebuCrust_api.Config;
+using CebuCrust_api.Interfaces;
+using CebuCrust_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CebuCrust_api.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly AppDbContext _db;
+        public CartRepository(AppDbContext db) => _db = db;
+
+        public async Task<List<Cart>> GetByUserAsync(int uid) =>
+            await _db.Carts.AsNoTracking()
+                           .Where(c => c.UserId == uid)
+                           .ToListAsync();
+
+        public async Task<Cart?> GetCartItemAsync(int uid, int pizzaId) =>
+            await _db.Carts.FindAsync(pizzaId, uid);
+
+        public async Task AddCartAsync(Cart cart)
+        {
+            _db.Carts.Add(cart);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateCartAsync(Cart cart)
+        {
+            _db.Carts.Update(cart);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteCartAsync(Cart cart)
+        {
+            _db.Carts.Remove(cart);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
