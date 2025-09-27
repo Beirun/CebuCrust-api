275fb2843c0397808f943243e29b38027fe6d456
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added IOrderRepository

---
+++ Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GetByUserAsync(int uid);
+        Task<List<Order>> GetAllAsync();
+        Task<Order> AddOrderAsync(Order order, IEnumerable<OrderList> items);
+        Task<Order?> GetByIdAsync(int orderId);
+        Task<List<OrderList>> GetOrderItemsAsync(int orderId);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(Order order);
+    }
+}
