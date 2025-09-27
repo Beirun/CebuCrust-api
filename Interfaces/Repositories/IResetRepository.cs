236beccc7f73b551534d62daad27f86744ba2286
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added IResetRepository

---
+++ Interfaces/Repositories/IResetRepository.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IResetRepository
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+        Task AddResetAsync(Reset reset);
+        Task<Reset?> GetValidResetAsync(string resetCode);
+        Task<User?> GetUserByIdAsync(int userId);
+        Task UpdateAsync(params object[] entities);
+    }
+}
