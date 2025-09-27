03cdd9d409da13bdf9088ac5e39ee6e741d1d94f
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:55 +0800

    Added IResetService

---
+++ Interfaces/Services/IResetService.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.ServiceModels;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IResetService
+    {
+        Task<bool> ResetRequestAsync(string email);
+
+        Task<bool> ResetVerifyAsync(string resetCode);
+
+        Task<bool> ResetPasswordAsync(string resetCode, string newPassword, string confirmPassword);
+    }
+}
