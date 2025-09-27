49d903f0895160dbc1091a3d79cc4b7003995c61
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:54 +0800

    Added ILocationService

---
+++ Interfaces/Services/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using CebuCrust_api.ServiceModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<IEnumerable<LocationResponse>> GetByUserAsync(int userId);
+        Task<LocationResponse> CreateAsync(int userId, LocationRequest request);
+        Task<LocationResponse?> UpdateAsync(int userId, int id, LocationRequest request);
+        Task<bool> DeleteAsync(int userId, int id);
+    }
+}
