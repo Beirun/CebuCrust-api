1ed57e1130095c1e41c9f6e9e97d8c60f5a1a54b
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:56 +0800

    Added PizzaRepository

---
+++ Repositories/PizzaRepository.cs
@@ -0,0 +1,44 @@
+﻿using CebuCrust_api.Config;
+using CebuCrust_api.Interfaces;
+using CebuCrust_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CebuCrust_api.Repositories
+{
+    public class PizzaRepository : IPizzaRepository
+    {
+        private readonly AppDbContext _db;
+        public PizzaRepository(AppDbContext db) => _db = db;
+
+        public async Task<List<Pizza>> GetAllAsync() =>
+            await _db.Pizzas.AsNoTracking().ToListAsync();
+
+        public async Task<Pizza?> GetByIdAsync(int id) =>
+            await _db.Pizzas.AsNoTracking().FirstOrDefaultAsync(p => p.PizzaId == id);
+
+        public async Task<Pizza> AddAsync(Pizza p)
+        {
+            _db.Pizzas.Add(p);
+            await _db.SaveChangesAsync();
+            return p;
+        }
+
+        public async Task<Pizza?> UpdateAsync(Pizza p)
+        {
+            _db.Pizzas.Update(p);
+            await _db.SaveChangesAsync();
+            return p;
+        }
+
+        public async Task<bool> DeleteAsync(Pizza p)
+        {
+            _db.Pizzas.Remove(p);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+    }
+}
