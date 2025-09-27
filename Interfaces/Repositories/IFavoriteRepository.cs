a597057892877bc318a1194a17dcbecf9a20aece
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:53 +0800

    Added IFavoriteRepository

---
+++ Interfaces/Repositories/IFavoriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.Models;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IFavoriteRepository
+    {
+        Task<List<Favorite>> GetByUserAsync(int uid);
+        Task<bool> ExistsAsync(int uid, int pizzaId);
+        Task AddFavoriteAsync(Favorite fav);
+        Task<Favorite?> GetFavoriteAsync(int uid, int pizzaId);
+        Task DeleteFavoriteAsync(Favorite fav);
+    }
+}
