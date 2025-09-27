e8fbcf33c7a8efcd27fb524cea834e699a29f2bd
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:54 +0800

    Added IFavoriteService

---
+++ Interfaces/Services/IFavoriteService.cs
@@ -0,0 +1,13 @@
+﻿using CebuCrust_api.ServiceModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CebuCrust_api.Interfaces
+{
+    public interface IFavoriteService
+    {
+        Task<IEnumerable<FavoriteResponse>> GetByUserAsync(int userId);
+        Task<bool> CreateAsync(int userId, FavoriteRequest request);
+        Task<bool> DeleteAsync(int userId, int pizzaId);
+    }
+}
