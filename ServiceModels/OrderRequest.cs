585d530c568fcc9b68d67cb259e4bd720894abde
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:50 +0800

    Updated OrderRequest model

--- ServiceModels/OrderRequest.cs
+++ ServiceModels/OrderRequest.cs
@@ -3,7 +3,6 @@ namespace CebuCrust_api.ServiceModels
 {
     public class OrderRequest
     {
-        public int UserId { get; set; }
         public int LocationId { get; set; }
         public string? OrderInstruction { get; set; }
         public string? OrderStatus { get; set; }
