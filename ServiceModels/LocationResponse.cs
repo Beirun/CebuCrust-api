f1dce2b3aebdee558189f254304094c2e61e0d29
Beirun <orillobernardjay@gmail.com>
2025-09-28 00:18:57 +0800

    Added LocationResponse model

---
+++ ServiceModels/LocationResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CebuCrust_api.ServiceModels
+{
+    public class LocationResponse
+    {
+        public int LocationId { get; set; }
+        public string? LocationCity { get; set; }
+        public string? LocationBrgy { get; set; }
+        public string? LocationStreet { get; set; }
+        public string? LocationHouseNo { get; set; }
+
+    }
+}
