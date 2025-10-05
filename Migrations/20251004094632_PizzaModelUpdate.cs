162598bf2504bec327d47e687b8e21ba82e04b74
Beirun <orillobernardjay@gmail.com>
2025-10-05 21:21:54 +0800

    Add migration 20251004094632_PizzaModelUpdate.cs

---
+++ Migrations/20251004094632_PizzaModelUpdate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuCrust_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PizzaModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Pizzas",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Pizzas");
+
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Pizzas");
+        }
+    }
+}
