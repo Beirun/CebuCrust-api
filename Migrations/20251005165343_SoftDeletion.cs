61c3a141959308626c2889a9d7606757a33bcf78
Beirun <orillobernardjay@gmail.com>
2025-10-07 16:02:15 +0800

    Add migration 20251005165343_SoftDeletion.cs

---
+++ Migrations/20251005165343_SoftDeletion.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuCrust_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SoftDeletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "Ratings",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "Notifications",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Notifications",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "Locations",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationLandmark",
+                table: "Locations",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationPostal",
+                table: "Locations",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Ratings");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "LocationLandmark",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "LocationPostal",
+                table: "Locations");
+        }
+    }
+}
