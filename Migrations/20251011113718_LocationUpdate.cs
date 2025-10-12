6906ad37d18462d71a8ff8e23f47f2acc6e87c0d
Beirun <orillobernardjay@gmail.com>
2025-10-12 16:55:50 +0800

    Add migration 20251011113718_LocationUpdate.cs

---
+++ Migrations/20251011113718_LocationUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuCrust_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class LocationUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDefault",
+                table: "Locations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDefault",
+                table: "Locations");
+        }
+    }
+}
