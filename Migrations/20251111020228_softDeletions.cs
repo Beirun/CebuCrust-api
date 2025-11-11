c3607c283fa6f93184c25a9aa708a64e0c920fa3
Beirun <orillobernardjay@gmail.com>
2025-11-11 11:54:24 +0800

    Add softDeletions migration

---
+++ Migrations/20251111020228_softDeletions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuCrust_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class softDeletions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Orders");
+        }
+    }
+}
