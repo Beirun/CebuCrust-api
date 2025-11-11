f61541bcb9fd0590f48ef9e1c8d4dda7f104ec45
Beirun <orillobernardjay@gmail.com>
2025-11-11 11:54:23 +0800

    Update OrderService

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -14,59 +14,51 @@ namespace CebuCrust_api.Services
         private readonly IOrderRepository _repo;
         public OrderService(IOrderRepository repo) => _repo = repo;
 
-        public async Task<IEnumerable<OrderResponse>> GetByUserAsync(int uid)
+        private OrderResponse Map(Order o, IEnumerable<OrderList>? items = null)
         {
-            var orders = await _repo.GetByUserAsync(uid);
-            return orders.Select(o => new OrderResponse
+            var lists = items ?? o.OrderLists;
+
+            return new OrderResponse
             {
                 OrderId = o.OrderId,
+                UserId = o.UserId,
                 LocationId = o.LocationId,
-                FirstName = o.User.UserFName,
-                LastName = o.User.UserLName,
-                PhoneNumber = o.User.UserPhoneNo,
+                FirstName = o.User?.UserFName,
+                LastName = o.User?.UserLName,
+                PhoneNumber = o.User?.UserPhoneNo,
                 OrderInstruction = o.OrderInstruction,
                 OrderStatus = o.OrderStatus,
                 OrderEstimate = o.OrderEstimate,
                 DateCreated = o.DateCreated,
-                OrderLists = o.OrderLists.Select(ol => new OrderItemResponse
+                Location = o.Location == null ? null : new LocationResponse
+                {
+                    LocationId = o.Location.LocationId,
+                    LocationCity = o.Location.LocationCity,
+                    LocationBrgy = o.Location.LocationBrgy,
+                    LocationStreet = o.Location.LocationStreet,
+                    LocationHouseNo = o.Location.LocationHouseNo,
+                    LocationPostal = o.Location.LocationPostal ?? "",
+                    LocationLandmark = o.Location.LocationLandmark ?? "",
+                    IsDefault = o.Location.IsDefault
+                },
+                OrderLists = lists.Select(ol => new OrderItemResponse
                 {
                     PizzaId = ol.PizzaId,
                     Quantity = ol.Quantity
                 }).ToList()
-            });
+            };
+        }
+
+        public async Task<IEnumerable<OrderResponse>> GetByUserAsync(int uid)
+        {
+            var orders = await _repo.GetByUserAsync(uid);
+            return orders.Select(o => Map(o));
         }
 
         public async Task<IEnumerable<OrderResponse>> GetAllAsync()
         {
             var orders = await _repo.GetAllAsync();
-            return orders.Select(o => new OrderResponse
-            {
-                OrderId = o.OrderId,
-                LocationId = o.LocationId,
-                FirstName = o.User.UserFName,
-                LastName = o.User.UserLName,
-                PhoneNumber = o.User.UserPhoneNo,
-                OrderInstruction = o.OrderInstruction,
-                OrderStatus = o.OrderStatus,
-                OrderEstimate = o.OrderEstimate,
-                DateCreated = o.DateCreated,
-                Location = new LocationResponse
-                {
-                    LocationId = o.Location.LocationId,
-                LocationCity = o.Location.LocationCity,
-                LocationBrgy = o.Location.LocationBrgy,
-                LocationStreet = o.Location.LocationStreet,
-                LocationHouseNo = o.Location.LocationHouseNo,
-                LocationPostal = o.Location.LocationPostal ?? "",
-                LocationLandmark = o.Location.LocationLandmark ?? "",
-                IsDefault = o.Location.IsDefault
-                },
-                OrderLists = o.OrderLists.Select(ol => new OrderItemResponse
-                {
-                    PizzaId = ol.PizzaId,
-                    Quantity = ol.Quantity
-                }).ToList()
-            });
+            return orders.Select(o => Map(o));
         }
 
         public async Task<OrderResponse> CreateAsync(int uid, OrderRequest request)
@@ -88,20 +80,7 @@ namespace CebuCrust_api.Services
             }).ToList();
 
             await _repo.AddOrderAsync(order, items);
-
-            return new OrderResponse
-            {
-                OrderId = order.OrderId,
-                LocationId = order.LocationId,
-                OrderInstruction = order.OrderInstruction,
-                OrderStatus = order.OrderStatus,
-                OrderEstimate = order.OrderEstimate,
-                OrderLists = items.Select(i => new OrderItemResponse
-                {
-                    PizzaId = i.PizzaId,
-                    Quantity = i.Quantity
-                }).ToList()
-            };
+            return Map(order, items);
         }
 
         public async Task<OrderResponse?> UpdateAsync(int pid, OrderRequest request)
@@ -120,24 +99,11 @@ namespace CebuCrust_api.Services
                 PizzaId = i.PizzaId,
                 Quantity = i.Quantity
             }).ToList();
-            
 
             await _repo.UpdateOrderAsync(existing, requestItems);
 
             var items = await _repo.GetOrderItemsAsync(pid);
-            return new OrderResponse
-            {
-                OrderId = existing.OrderId,
-                LocationId = existing.LocationId,
-                OrderInstruction = existing.OrderInstruction,
-                OrderStatus = existing.OrderStatus,
-                OrderEstimate = existing.OrderEstimate,
-                OrderLists = items.Select(i => new OrderItemResponse
-                {
-                    PizzaId = i.PizzaId,
-                    Quantity = i.Quantity
-                }).ToList()
-            };
+            return Map(existing, items);
         }
 
         public async Task<OrderResponse?> UpdateStatusAsync(int orderId, string status)
@@ -149,19 +115,7 @@ namespace CebuCrust_api.Services
             await _repo.UpdateOrderAsync(existing);
 
             var items = await _repo.GetOrderItemsAsync(orderId);
-            return new OrderResponse
-            {
-                OrderId = existing.OrderId,
-                LocationId = existing.LocationId,
-                OrderInstruction = existing.OrderInstruction,
-                OrderStatus = existing.OrderStatus,
-                OrderEstimate = existing.OrderEstimate,
-                OrderLists = items.Select(i => new OrderItemResponse
-                {
-                    PizzaId = i.PizzaId,
-                    Quantity = i.Quantity
-                }).ToList()
-            };
+            return Map(existing, items);
         }
 
         public async Task<bool> DeleteAsync(int orderId)
