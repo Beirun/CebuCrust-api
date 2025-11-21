6ffdaac957c86c979feb82b99f088d8d240e313b
Beirun <orillobernardjay@gmail.com>
2025-11-21 18:02:25 +0800

    Add updatePizza migration

---
+++ Migrations/20251121063504_updatePizza.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuCrust_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePizza : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsAvailable",
+                table: "Pizzas",
+                newName: "Stock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Stock",
+                table: "Pizzas",
+                newName: "IsAvailable");
+        }
+    }
+}
